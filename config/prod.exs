309895245a645c20669cd815670d43701b56cff8
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-10-27 19:13:48 -0500

    Heroku changes

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,10 @@
 use Mix.Config
 
+config :beermusings, Beermusings.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 2
+
 # For production, we configure the host to read the PORT
 # from the system environment. Therefore, you will need
 # to set PORT=80 before running your server.
